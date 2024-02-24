d5050ebe18819cfbbde03aef8914a8942d267288
Ashutosh lakshakar <lakshakarashutosh@gmail.com>
2024-02-24 13:32:41 +0530

    feat:create vite and started implementation

---
+++ marbleai/src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='border-2 h-[50vh] border-red-700'>
+          
+          
+      </div>
+    </>
+  )
+}
+
+export default App
